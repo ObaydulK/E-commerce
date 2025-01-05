6adeb9a56e5442ed4e0701e998ea712237375e25
Obaydul_Kuasha <obaydul00000@gmail.com>
2025-01-05 08:37:35 +0600

    first commit

---
+++ resources/views/livewire/item-card.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="bg-gray-100">
+        <div>
+            <div>
+                <img src="https://images.unsplash.com/photo-1592921870789-04563d55041c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                    alt="product-images" class="aspect-square w-full rounded object-cover" />
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/livewire/product-section.blade.php
@@ -0,0 +1,14 @@
+<div class="px-10 md:px-20">
+    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+
+    <div>
+        <h2 class="font-medium text-[20px]">Brand New </h2>
+        <livewire:product-iteam />
+
+    </div>
+
+
+
+
+
+</div>
