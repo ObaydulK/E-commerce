a6c4d0fbb48433746da2d70442e46b409c76d0ad
Obaydul_Kuasha <obaydul00000@gmail.com>
2025-01-11 11:18:21 +0600

    Update my product table and colluct db data

--- app/Livewire/ProductTable.php
+++ app/Livewire/ProductTable.php
@@ -3,11 +3,39 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Product;
+use Livewire\WithPagination;
 
 class ProductTable extends Component
 {
+    use WithPagination;
+    public $perPage=5;
+    public $sortBy= "Created_at";
+    public $sortDir= "DESC";
+    public $search= " ";
+    public $currentUrl;
+
+
+    public function setSortBy($sortColum){
+        if($this->sortBy == $sortColum){
+            $this->sortDir = ($this->sortDir == 'ASC') ? 'DESC' : 'ASC';
+            return;       
+        }
+        $this->sortBy = $sortColum;
+        $this->sortDir = 'ASC';
+
+    }
+
     public function render()
     {
-        return view('livewire.product-table');
+        $current_Url = url()->current();
+        $explode_url = explode('/', $current_Url);
+        
+        $this->currentUrl = $explode_url[3];
+        
+
+        return view('livewire.product-table',[
+            'products' => Product::with('category')->search($this->search)->orderBy($this->sortBy,$this->sortDir)->paginate($this->perPage)
+        ]);
     }
 }
